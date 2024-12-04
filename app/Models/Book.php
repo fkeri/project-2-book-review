f8662dcc544402290682c4b7ae723c3564deaffc
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-12-04 13:46:42 +0100

    Factories & Seeders (2.5)

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    use HasFactory;
+
     // Define one-to-many relationship between 'books' & 'reviews' tables
     public function reviews()
     {
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    use HasFactory;
+
     // Define inverse side of one-to-many relationship between 'books' & 'reviews' tables
     public function book() {
         return $this->belongsTo(Book::class);
