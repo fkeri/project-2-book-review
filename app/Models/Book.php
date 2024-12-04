955cd6c3f1bcffeb46bd9da759b7fbfbbe58a3b8
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-12-04 13:11:50 +0100

    Define One-to-Many Relationship between Book & Review Models (2.4)

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    // Define one-to-many relationship between 'books' & 'reviews' tables
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    // Define inverse side of one-to-many relationship between 'books' & 'reviews' tables
+    public function book() {
+        return $this->belongsTo(Book::class);
+    }
 }
