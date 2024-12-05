d036ed4a35872fc9863db2bac7f0f2bef7fbe669
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-12-05 16:22:02 +0100

    Scoped Resource Controller - Adding a Review (2.22)

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -25,5 +25,8 @@ class Review extends Model
         static::deleted(
             fn(Review $review) => cache()->forget('book:' . $review->book_id)
         );
+        static::created(
+            fn(Review $review) => cache()->forget('book:' . $review->book_id)
+        );
     }
 }
